cecb6d0f13ff74ace3fb716327fef3c8412aa833
silasferreiracarneiro <silasferreira96@hotmail.com>
2019-05-12 20:35:44 -0300

    Feito a tela de home e cadastro

--- app/src/main/java/com/example/trailerfix/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/trailerfix/ui/login/LoginActivity.kt
@@ -1,13 +1,25 @@
 package com.example.trailerfix.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.trailerfix.R
+import com.example.trailerfix.ui.home.HomeActivity
+import com.example.trailerfix.ui.register.RegisterActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+
+        txtCadastre.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
